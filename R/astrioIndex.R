347f6de972a95fd7d3194bc20778c92a7f99f8e8
Marco De Zuani <marcodezuani@Marcos-MacBook-Air.local>
2020-02-03 14:07:10 +0100

    Add first files

---
+++ R/astrioIndex.R
@@ -0,0 +1,66 @@
+astrioIndex<- function(fcs, out_file = TRUE, out_name="Indexed_file.fcs"){ 
+  rows<-1:nrow(fcs)
+  XYindex<-matrix(nrow = nrow(fcs), ncol = 2)
+  colnames(XYindex)<-c("X-index","Y-index")
+
+  a<-1:6
+  b<-7:32
+  zero<-raw(32)
+  
+  for(m in rows){
+    #Reset X1 and Y1 values
+    X1<-raw(1)
+    Y1<-raw(1)
+  
+    # Extract m-th "Sort Classifier" value
+    sortcl<-exprs(fcs)[m,'Sort Classifier']
+    bit<-intToBits(sortcl)
+  
+    # Extract the X-parameter bits
+    for(i in a){
+      X1[i]<-bit[20+i]
+    }
+  
+    # Add 26 zeroes to complete binary
+    for(n in b){
+      X1[n]<-zero[n]
+    }
+  
+    # Invert X1 and convert to integer
+    rev(X1)
+    X<-packBits(X1, "integer")
+  
+    # Extract the Y-parameter bits
+    for(i in a){
+      Y1[i]<-bit[26+i]
+    }
+  
+    # Add 26 zeroes to complete binary
+    for(n in b){
+      Y1[n]<-zero[n]
+    }
+  
+    # Invert X1 and convert to integer
+    rev(Y1)
+    Y<-packBits(Y1, "integer")
+  
+    # If Y is not zero (=unsorted), than subtract its value to 9
+    if(Y!=0){
+      Y<-9-Y
+    }
+  
+    # Stores X and Y indexes in the m row of the matrix
+    X<-X*10000
+    Y<-Y*10000
+  
+    XYindex[m,"X-index"]<-X
+    XYindex[m,"Y-index"]<-Y
+    
+  }
+  fcs2<-cbind2(fcs, XYindex)
+  fcs2@description$`$FIL`<-paste0("indexed_", fcs2@description$`$FIL`)
+  fcs2@description$FILENAME<-paste0("indexed_", fcs2@description$FILENAME)
+  if(out_file==TRUE)
+  write.FCS(fcs2, filename = as.character(out_name))
+  return("Indexed!")
+}
