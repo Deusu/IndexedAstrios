627f2e3f8f1349e23a37cd9f45c06af9e204250b
Deusu <marco.de.zuani@gmail.com>
2020-02-03 18:24:59 +0100

    Add funcion to index fcs files

---
+++ R/astrioIndex_fcs.R
@@ -0,0 +1,67 @@
+astrioIndex_fcs<- function(fcs, out_file = TRUE, out_name="Indexed_file.fcs"){
+  ff<-flowCore::read.FCS(as.character(fcs))
+  rows<-1:nrow(ff)
+  XYindex<-matrix(nrow = nrow(ff), ncol = 2)
+  colnames(XYindex)<-c("X-index","Y-index")
+
+  a<-1:6
+  b<-7:32
+  zero<-raw(32)
+  
+  for(m in rows){
+    #Reset X1 and Y1 values
+    X1<-raw(1)
+    Y1<-raw(1)
+  
+    # Extract m-th "Sort Classifier" value
+    sortcl<-exprs(ff)[m,'Sort Classifier']
+    bit<-intToBits(sortcl)
+  
+    # Extract the X-parameter bits
+    for(i in a){
+      X1[i]<-bit[20+i]
+    }
+  
+    # Add 26 zeroes to complete binary
+    for(n in b){
+      X1[n]<-zero[n]
+    }
+  
+    # Invert X1 and convert to integer
+    rev(X1)
+    X<-packBits(X1, "integer")
+  
+    # Extract the Y-parameter bits
+    for(i in a){
+      Y1[i]<-bit[26+i]
+    }
+  
+    # Add 26 zeroes to complete binary
+    for(n in b){
+      Y1[n]<-zero[n]
+    }
+  
+    # Invert X1 and convert to integer
+    rev(Y1)
+    Y<-packBits(Y1, "integer")
+  
+    # If Y is not zero (=unsorted), than subtract its value to 9
+    if(Y!=0){
+      Y<-9-Y
+    }
+  
+    # Stores X and Y indexes in the m row of the matrix
+    X<-X*10000
+    Y<-Y*10000
+  
+    XYindex[m,"X-index"]<-X
+    XYindex[m,"Y-index"]<-Y
+    
+  }
+  fcs2<-cbind2(ff, XYindex)
+  fcs2@description$`$FIL`<-paste0("indexed_", fcs2@description$`$FIL`)
+  fcs2@description$FILENAME<-paste0("indexed_", fcs2@description$FILENAME)
+  if(out_file==TRUE)
+  write.FCS(fcs2, filename = as.character(out_name))
+  return("Indexed!")
+}
